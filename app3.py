820db0c5e1ed89fe13c1a03897e06ef65381e50d
Tanguy Robilliard <48035168+Tanguyrob@users.noreply.github.com>
2019-07-22 16:57:22 +0200

    hack

--- app3.py
+++ app3.py
@@ -7,10 +7,5 @@ app = Flask(__name__)
 def hello():
     return render_template("hello.html")
 
-@app.route("/second")
-def seconde():
-    return "https://www.developpez.net/forums/d584296/webmasters-developpement-web/balisage-x-html-validation-w3c/html-chemin-absolu-d-image/"
-
-
 if __name__== "__main__":
     app.run()
