ccddc7ee88d90f32c3dff46dbd14fdc003d4147a
Tanguy Robilliard <48035168+Tanguyrob@users.noreply.github.com>
2019-07-22 17:18:05 +0200

    nex style

--- app3.py
+++ app3.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/kali")
 def hello():
     return render_template("hello.html")
+
+@app.route("/")
+def effets():
+    return render_template("thirdPage.html")
 
 if __name__== "__main__":
     app.run()
