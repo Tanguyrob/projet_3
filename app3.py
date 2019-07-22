167ec98d3e6d1bbae2fc6dc87725a93a76e867bc
Tanguy Robilliard <48035168+Tanguyrob@users.noreply.github.com>
2019-07-22 17:36:54 +0200

    effet light 12

--- app3.py
+++ app3.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/kali")
-def hello():
-    return render_template("hello.html")
-
 @app.route("/")
 def effets():
     return render_template("thirdPage.html")
+
+@app.route("/kali")
+def hello():
+    return render_template("hello.html")
 
 if __name__== "__main__":
     app.run()
